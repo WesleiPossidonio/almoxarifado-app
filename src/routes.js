60b7af7e7a4f8936d6d2b0aa1a2f615a58781ac2
Weslei Possidonio <weslei.possidonio@gmail.com>
2026-01-24 18:05:37 -0300

    teste pr

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express'
 
 const routes = new Router()
+console.log('Routes file loaded')
 
+// Defina suas rotas aqui
+routes.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 export default routes
