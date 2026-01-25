55c0592d6a7fdb47ddd7c5ef0d882853ce6c5d51
Weslei Possidonio <weslei.possidonio@gmail.com>
2026-01-25 01:00:13 -0300

    feat:created migration clients

---
+++ src/database/migrations/20260125035045-create-clients.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('clients', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('clients')
+  },
+}
