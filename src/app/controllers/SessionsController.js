9d2ade1ce220214d4309dedade0bcc295b99f507
Weslei Possidonio <weslei.possidonio@gmail.com>
2026-02-04 16:58:51 -0300

    fix: update import auth.js SessionsController

--- src/app/controllers/SessionsController.js
+++ src/app/controllers/SessionsController.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup'
 import jwt from 'jsonwebtoken'
-import authConfig from '../../config/auth..js'
+import authConfig from '../../config/auth.js'
 import User from '../models/Users.js'
 import Client from '../models/Clients.js'
 class SessionController {
